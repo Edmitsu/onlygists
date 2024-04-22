e4ea42a87e6ebb87a4a199fb275fd775c1f09870
Edmitsu <ed.barcauscas@gmail.com>
2024-04-22 12:52:58 -0300

    add new module reports and widgetGroups

---
+++ modules/reports/components/Widget/Condensed/Condensed.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value: numer | string
+  label: string
+}>()
+</script>
+<template>
+  <div class="flex flex-1 flex-col bg-white p-5 rounded-xl">
+    <span class="font-[Inter] text-4xl text-primary-500 font-bold">{{ props.value }}</span>
+    <p class="font-[Inter] text-lg text-gray-700">{{ props.label }}</p>
+  </div>
+</template>
--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/users/components/PublicHeadline/PublicHeadline.vue'
+import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue'
+import WidgetGroupLoader from '@/modules/reports/components/Widget/Group/Loader.vue'
+import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue'
 </script>
 
 <template>
   <PublicHeadline />
+
+  <WidgetGroup>
+    <WidgetGroupLoader :loading="false" :amount="3">
+      <WidgetCondensed :value="10" label="Gists do total"></WidgetCondensed>
+      <WidgetCondensed :value="5" label="Gists gratuitos"></WidgetCondensed>
+      <WidgetCondensed :value="5" label="Gists pagos"></WidgetCondensed>
+    </WidgetGroupLoader>
+  </WidgetGroup>
 </template>
